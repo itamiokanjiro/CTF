545c5db5fb35ade5dbd1ccdf4da27bc782ee73a8
hacker123 <a234123234a@gmail.com>
2025-05-22 23:13:06 +0000

    Init CTF

---
+++ Glory of the Garden/VaultDoorTraining2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class VaultDoorTraining2 {
+    public static void main(String args[]) {
+        String flag = "picoCTF{" + "w4rm1ng_Up_w1tH_jAv4_eec0716b713" + "}";
+        System.out.println("Vault password is: " + flag);
+    }
+}
+
